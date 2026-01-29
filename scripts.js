c5ab0a205be2957b03dcddf3f7df16b628c5f7e0
Alex <adavisson@ewu.edu>
2026-01-28 23:46:57 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const hamburger = document.getElementsByClassName('hamburger');
 const main = document.getElementsByClassName('mainContainer');
 
-hamburger.addEventListener('click', function() {
-  main.classList.add("blurred");
-});
+hamburger.addEventListener(click, function(){
+    main.classList.toggle("blurred");
+})
