9f6ec8622e53e49521cd37fa5ef8f2aec791b4fb
Alex <adavisson@ewu.edu>
2026-01-29 00:09:32 -0800

    n

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 const hamburger = document.getElementsByClassName('hamburger');
 const main = document.getElementsByClassName('mainContainer');
-const menu = document.getElementsByClassName('menu');
+const menu = document.getElementsByid('menu');
 
 hamburger[0].addEventListener("click", function(){
     main[0].classList.toggle("blurred");
+    menu[0].classList.toggle("noblur");
 })
