394b2cc0e9b53781c04f61632ab9c4cfff09f02a
Alex <adavisson@ewu.edu>
2026-01-28 23:00:14 -0800

    Revert "added js blur"
    This reverts commit 7f5a3589da3b312bca3ebad3e4d1f13782e5cb46.

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleBlur(state) {
-    const modalOverlay = document.getElementById('modal-overlay');
-    if (state) {
-        modalOverlay.style.display = 'flex'; /* Show the overlay and blur */
-    } else {
-        modalOverlay.style.display = 'none'; /* Hide the overlay and unblur */
-    }
-}
